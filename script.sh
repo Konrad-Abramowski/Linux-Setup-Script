3447962336bdcc294eb53b8edb41649060484ddc
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-02-05 23:46:53 +0100

    Fix pgadmin4 installation

--- script.sh
+++ script.sh
@@ -20,9 +20,24 @@ while read -r p; do sudo apt-get install -y $p ; done < <(cat << "EOF"
     firefox
     chromium-browser
     obs-studio
-    pgadmin4
+    curl
 EOF
 )
+
+### pgadmin4
+
+# Install the public key for the repository:
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+# Create the repository configuration file:
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+#
+# Install pgAdmin
+#
+
+# Install for both desktop and web modes:
+sudo apt install pgadmin4 -y
 
 
 # jdk
