5206b18c4c322d67d9cfade0f57add7b2e0e8501
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-07-25 12:15:29 +0200

    Fix

--- scripts/root-script.sh
+++ scripts/root-script.sh
@@ -6,7 +6,6 @@ source ./scripts/root-script-functions.sh
 # script body
 update_ubuntu_and_packages
 
-install_pygrid
 install_applications_with_snap
 install_applications_with_apt-get
 install_pgadmin4
